418fa6e64b5a756a4e9470e8664417ddc481a89a
ray <87890685+rayjynx@users.noreply.github.com>
2021-12-15 22:58:53 +0530

    Main Menu update

---
+++ Assets/Scripts/mainmenu/quitbutton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitbutton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }   
+}
